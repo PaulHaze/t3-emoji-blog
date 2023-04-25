8eb3e62b9ede0870778b82435a57fa073376a5f0
PaulHaze <dev@artemist.com.au>
2023-04-25 16:10:00 +1000

    sign in tweaks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppType } from 'next/app';
+import Head from 'next/head';
 import { ClerkProvider } from '@clerk/nextjs';
 import { api } from '@/utils/api';
 
@@ -7,6 +8,11 @@ import '@/styles/main.scss';
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Emoji Blog</title>
+        <meta name="description" content="t3 emoji blog tutorial" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </ClerkProvider>
   );
